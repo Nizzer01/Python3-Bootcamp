e8cf27e4b0bc43e840668ef4f997732c0d77e549
Gav Shields <65550958+Nizzer01@users.noreply.github.com>
2024-08-07 13:33:40 +0100

    Looping

---
+++ Looping/for.py
@@ -0,0 +1,15 @@
+# for x in range(1,10):
+# 	print(x)
+# 	print(x*x)
+
+for letter in "coffee":
+	print(letter*10)
+
+for x in range(1, 10):
+	print(x)
+
+for x in range(1, 10, 2):
+	print(x)
+
+for x in range(7, 0, -1):
+	print(x)
