965ac13a4ff3747ea80acf61785caf5fb1abd40a
Gav Shields <65550958+Nizzer01@users.noreply.github.com>
2024-08-07 16:38:00 +0100

    Lists overview

---
+++ Lists/append_insert_extend.py
@@ -0,0 +1,15 @@
+# Lists Basics Exercise
+# Now that we've learned about lists, let's get some practice. See the problem statements in the comments below:
+
+
+# Initially create an empty list called instructors
+
+# Add the following strings to the instructors list
+    # "Colt"
+    # "Blue"
+    # "Lisa"
+instructors = []
+instructors.append("Colt")
+instructors.append("Blue")
+instructors.append("Lisa")
+# Run the tests to make sure you've done this correctly!
---
+++ Lists/create_lists.py
@@ -0,0 +1,16 @@
+# Define 2 separate lists:
+#
+# First, define a list called my_stuff .
+# It must be be at least 4 elements long.  The data is completely up to you,
+# but it must contain at least 1 string and 1 float.
+#
+# Next, define a list called nums . It should be a list
+# containing all the numbers between 1 and 99 (including 99).
+# Don't type this out manually! Use what we learned at the end of the previous video!
+
+
+# Define my_stuff
+my_stuff = [1, "test", 6.55, 42]
+
+# Define nums
+nums = list(range(1,100))
